8bdc49360782af445430984788544c6d756dc6f1
oliinyk-alina-ip52 <oleynik.a.o@gmail.com>
2016-03-30 23:23:54 +0300

    change

--- 7.2/7.2/lab7.cpp
+++ 7.2/7.2/lab7.cpp
@@ -6,23 +6,25 @@
 using namespace std;
 
 const int n = 100;
-char s[n];
-char a[n],c;
 void word();
 void inp_symbols();
+char a[50];
+char s[n];
+int k=0,d,b;
 
 int main()
 {
 	printf("enter string with symbols : ");
 	inp_symbols();
 	printf("enter string with words: ");
-	
+	do{
+		word();
+	} while (a != "quit");
 	system("pause");
 }
 
 void inp_symbols()
 {
-	int k = 0;
 	do
 	{
 		s[k] = _getche();
@@ -30,4 +32,23 @@ void inp_symbols()
 	} while (s[k - 1] != 27);
 	k--;
 	printf("\n");
+}
+
+void word()
+{
+	int m;
+	cin.getline(a, 50);
+	for (int i = 0; i < k; i++)
+	{
+		if (a[0] == s[i])
+			d = 1;
+	}
+	if (d==1)
+	for (int i = 0; i < k;i++)
+	if (a[strlen(a) - 1] == s[i])
+		b = 1;
+	if (d == 1 && b == 1)
+		printf("\n%s", a);
+	d = 0;
+	b = 0;
 }
