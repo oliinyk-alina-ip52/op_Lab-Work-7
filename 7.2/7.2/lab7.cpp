f2fa65b20267c1822c68b5548c8e96ca82d57937
oliinyk-alina-ip52 <oleynik.a.o@gmail.com>
2016-04-06 23:41:06 +0300

    documentation

--- 7.2/7.2/lab7.cpp
+++ 7.2/7.2/lab7.cpp
@@ -2,6 +2,7 @@
 #include<cstring>
 #include<stdio.h>
 #include<conio.h>
+#include<string.h>
 
 using namespace std;
 
@@ -10,16 +11,18 @@ void word(char *, char*);
 void inp_symbols(char[]);
 char a[n];
 char s[n];
-int k=0,d,b;
+char b[5] = " ,." ;
+int k=0,d,c,m=0;
+typedef char o[n][n];
+o mas;
 
 int main()
 {
-	printf("enter string with symbols : ");
-	inp_symbols(a);
 	printf("enter string with words: ");
-	do{
-		word(s,a);
-	} while (s[k-1] !=27);
+	cin.getline(s, 50);
+	printf("\nenter string with symbols : ");
+	inp_symbols(a);
+	word(s, a);
 	system("pause");
 }
 
@@ -34,21 +37,40 @@ void inp_symbols(char q[n] )
 	printf("\n");
 }
 
+/*void words(char *w)
+{
+	char *tok;
+	char *next = NULL;
+	char o[n][n];
+	for (tok = strtok_s(w, b, &next); tok; tok = strtok_s(NULL, b, &next)){
+		strcpy(o[m], tok);
+		m++;
+	}
+}*/
+
 void word(char *p, char*t)
 {
-	int m;
-	cin.getline(p, 50);
-	for (int i = 0; i < k; i++)
-	{
-		if (p[0] == t[i])
-			d = 1;
+	char *tok;
+	char *next = NULL;
+	for (tok = strtok_s(p, b, &next); tok; tok = strtok_s(NULL, b, &next)){
+		strcpy_s(mas[m], tok);
+		m++;
+}
+	/*for (int i = 0;i < m;i++)
+		printf("%s\n", mas[i]);*/
+	for (int j = 0; j < m; j++){
+		for (int i = 0; i < k; i++)
+		{
+			if (mas[j][0] == t[i])
+				d = 1;
+		}
+		if (d == 1)
+		for (int i = 0; i < k; i++)
+		if (mas[j][strlen(mas[j]) - 1] == t[i])
+			c = 1;
+		if (d == 1 && c == 1)
+			printf("%s\n", mas[j]);
+		d = 0;
+		c = 0;
 	}
-	if (d==1)
-	for (int i = 0; i < k;i++)
-	if (p[strlen(p) - 1] == t[i])
-		b = 1;
-	if (d == 1 && b == 1)
-		printf("%s\n", p);
-	d = 0;
-	b = 0;
 }
